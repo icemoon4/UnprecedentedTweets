d4552dc42768f0ff1664a0347af9a952f7f018b3
Rachel Palmer <palmerr@wit.edu>
2020-11-08 09:10:57 -0500

    added sentiment script by julia

---
+++ sentimenttest.py
@@ -0,0 +1,49 @@
+from google.cloud import language_v1
+import csv
+
+def sample_analyze_sentiment(text_content):
+    """
+    Analyzing Sentiment in a String
+
+    Args:
+      text_content The text content to analyze
+    """
+
+    client = language_v1.LanguageServiceClient()
+
+    # text_content = 'I am so happy and joyful.'
+
+    # Available types: PLAIN_TEXT, HTML
+    type_ = language_v1.Document.Type.PLAIN_TEXT
+
+    # Optional. If not specified, the language is automatically detected.
+    # For list of supported languages:
+    # https://cloud.google.com/natural-language/docs/languages
+    language = "es"
+    document = {"content": text_content, "type_": type_, "language": language}
+
+    # Available values: NONE, UTF8, UTF16, UTF32
+    encoding_type = language_v1.EncodingType.UTF8
+
+    response = client.analyze_sentiment(request = {'document': document, 'encoding_type': encoding_type})
+    return response 
+
+def main():
+    with open('collectedtweets.csv', encoding='utf8') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                print(f'Column names are {", ".join(row)}')
+                line_count += 1
+            else:
+                response = sample_analyze_sentiment(row[3])
+                print(row[3])
+                print(u"Tweet sentiment score: {}".format(response.document_sentiment.score))
+                print(u"Tweet sentiment magnitude: {}".format(response.document_sentiment.magnitude))
+                print()
+
+
+if __name__=="__main__":
+    main()
+
